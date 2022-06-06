552540004e7c5c573fea551ad87bf1c10b9dec71
Andy <marmon.ell.x@gmail.com>
2022-06-06 23:37:13 +0300

    Add allure reports

--- src/test/java/tests/CalendarTest.java
+++ src/test/java/tests/CalendarTest.java
@@ -1,12 +1,10 @@
 package tests;
 
 import dto.QuickAddWorkout;
-import lombok.extern.log4j.Log4j2;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-@Log4j2
 public class CalendarTest extends BaseTest{
 
 
